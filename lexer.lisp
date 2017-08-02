7aea31721889355657fc4a68a912d326b570c3c2
Christopher H Cope <copecd@gmail.com>
2017-08-02 17:29:11 -0600

    Figuring out general program structure still.

--- lexer.lisp
+++ lexer.lisp
@@ -4,14 +4,20 @@
 
 ;;; "lexer" goes here. Hacks and glory await!
 
+
+;; Classes
+
 ;; https://en.wikipedia.org/wiki/Automata_theory
-(defclass finite-automaton ()
+(defclass finite-automata ()
   ((Q :initarg :Q
-      :initform (error "Must supply an initial Q.")
+      :initform (make-array 10
+			    :initial-element nil
+			    :adjustable t
+			    :fill-pointer 0)
       :accessor Q
       :documentation "A finite set of states Q.")
    (Sigma :initarg :Sigma
-	  :initform (error "Must supply an initial Σ.")
+	  :initform 'cl-utf
           :accessor Sigma
           :documentation "A finite set of input symbols Σ.")
    (Delta :initarg :Delta
@@ -29,31 +35,26 @@
   (:documentation "A Finite Automaton is represented formally by a 5-tuple: (Q, Σ, Δ, q_0, F)."))
     
 ;; https://en.wikipedia.org/wiki/Nondeterministic_finite_automaton
-(defclass nfa (finite-automaton) ()
+(defclass nfa (finite-automata) ()
   (:documentation "A Nondeterministic Finite Automaton."))
 
 ;; https://en.wikipedia.org/wiki/Deterministic_finite_automaton
-(defclass dfa (finite-automaton) ()
+(defclass dfa (finite-automata) ()
   (:documentation "A Deterministic Finite Automaton."))
 
-(defun make-automaton (class
-		       &rest rest
-		       &key
-			 (Q (make-Q))
-			 (Sigma (make-Sigma))
-			 (Delta (make-Delta))
-			 (q_0 (make-q_0))
-			 (F (make-F)))
-  (case class
-    (finite-automaton
-     (make-instance 'finite-automaton :Q Q :Sigma Sigma :Delta Delta :q_0 q_0 :F F))
 
-(defun make-Q (&key (size-of-q 10))
+;; Creation of automaton objects
+
+(defun make-Q-vector (&key (size-of-q 10))
   (make-array size-of-q
 	      :initial-element nil
 	      :adjustable t
 	      :fill-pointer 0))
-	
+
+(defun make-Q (class &rest keys)
+  (case class
+    (finite-automata 
+
 (defun make-Sigma ()
   'Sigma-slot)
 
@@ -65,3 +66,49 @@
 
 (defun make-F ()
   'F-slot)
+
+(defun make-finite-automaton (&key
+				(Q (make-Q 'finite-automata))
+				(Sigma (make-Sigma 'finite-automata))
+				(Delta (make-Delta 'finite-automata))
+				(q_0 (make-q_0 'finite-automata))
+				(F (make-F 'finite-automata)))
+  (make-instance 'finite-automata
+		 :Q Q
+		 :Sigma Sigma
+		 :Delta Delta
+		 :q_0 q_0
+		 :F F))
+
+(defun make-nfa (&key
+		   (Q (make-Q 'nfa))
+		   (Sigma (make-Sigma 'nfa))
+		   (Delta (make-Delta 'nfa))
+		   (q_0 (make-q_0 'nfa))
+		   (F (make-F 'nfa)))
+  (make-instance 'nfa
+		 :Q Q
+		 :Sigma Sigma
+		 :Delta Delta
+		 :q_0 q_0
+		 :F F))
+
+(defun make-dfa (&key
+		   (Q (make-Q 'nfa))
+		   (Sigma (make-Sigma 'nfa))
+		   (Delta (make-Delta 'nfa))
+		   (q_0 (make-q_0 'nfa))
+		   (F (make-F 'nfa)))
+  (make-instance 'dfa
+		 :Q Q
+		 :Sigma Sigma
+		 :Delta Delta
+		 :q_0 q_0
+		 :F F)) 
+
+(defun make-automaton (class &rest class-initarg-keys)
+  (case class
+    (finite-automaton (make-finite-automaton class-initarg-keys))
+    (nfa (make-nfa class-initarg-keys))
+    (dfa (make-dfa class-initarg-keys))
+    (otherwise (make-instance class class-initarg-keys)))) ;just act like make-instance and perhaps error
