a0e00e5a9b1b17875bf9b6439ee0fa36622a016b
Christopher H Cope <copecd@gmail.com>
2017-08-11 16:44:49 -0600

    experiments...

--- lexer-refactor.lisp
+++ lexer-refactor.lisp
@@ -8,167 +8,103 @@
 ;;   an initial (or start) state q0 ∈ Q
 ;;   a set of states F distinguished as accepting (or final) states F ⊆ Q.
 
-(defclass states ()
-  ((preface :initarg :preface  :initform "q_"             :reader preface)
-   (iterate :initarg :iterate  :initform 0                :reader iterate)))
+(defclass preface ()
+  ((preface :initarg :preface
+	    :reader preface)))
 
-(defclass FA (states)
-  ((Q       :initarg :Q        :initform (make-vector 1)  :reader Q)
-   (Sigma   :initarg :Sigma    :initform 'cl-utf          :reader Sigma)
-   (Delta   :initarg :Delta    :initform (make-vector 1)  :reader Delta)
-   (q_0     :initarg :q_0                                 :reader q_0)
-   (F       :initarg :F        :initform (make-vector 1)  :reader F)))
+(defclass iterate ()
+  ((iterate :initarg :iterate
+            :reader iterate)))
 
-(defun make-vector (size &key (initial-element nil) (adjustable t) (fill-pointer 1))
-  (make-array size :initial-element initial-element
-		   :adjustable adjustable
-		   :fill-pointer fill-pointer))
+(defclass states-names (preface iterate)
+  ((preface :initform "q_")
+   (iterate :initform 0)))
 
-(defmethod make-Delta ((Sigma (eql 'cl-utf)))
+(defclass Q ()
+  ((Q :initarg :Q
+      :initform (make-vector 1)
+      :reader Q)))
+
+(defclass Sigma ()
+  ((Sigma :initarg :Sigma
+	  :initform 'cl-utf
+	  :reader Sigma)))
+
+(defclass Delta ()
+  ((Delta :initarg :Delta
+	  :initform (make-vector 1)
+	  :reader Delta)))
+
+;;Q is assumed as vector--but need to know Sigma to make Delta transitions function.
+(defclass Sigma-Delta (Sigma Delta) ())
+
+(defclass q_0 ()
+  ((q_0 :initarg q_0
+	:reader q_0)))
+
+(defclass F ()
+  ((F :initarg F
+      :initform (make-vector 1)
+      :reader F)))
+
+(defclass FA (Q Sigma-Delta q_0 F states-names) ())
+
+(defun make-vector (size &key (initial-element nil) (adjustable t) (fill-pointer 0))
+  (make-array size
+	      :initial-element initial-element
+	      :adjustable adjustable
+	      :fill-pointer fill-pointer))
+
+(defgeneric make-Delta (Sigma-type)
+  (:documentation "Create respective character-transition map data-structure."))
+
+(defmethod make-Delta ((Sigma-type (eql 'cl-utf)))
   (make-hash-table :test 'eql)) ;eql for cl char's
+
+(defgeneric make-state (states-names)
+  (:documentation "Name a new state from a reference class instance for a series of states."))
 	      
-(defmethod make-state ((states states))
-  (with-slots (preface iterate) states
+(defmethod make-state ((states-names states-names))
+  (with-slots (preface iterate) states-names
     (format nil "~a~d" preface (1- (incf iterate)))))
 
 (defmethod initialize-instance :after ((FA FA) &key &allow-other-keys)
-  (push-state 'next FA :q_0-p t))
+  (push-state FA FA :start-p t))
 
-(defmethod push-state (state (FA FA) &key (q_0-p nil) (F-p nil))
-  (with-slots (Q Sigma Delta q_0 F) FA
-    (vector-push-extend state Q)
-    (vector-push-extend (make-Delta Sigma) Delta)
-    (when q_0-p
-      (setf q_0 state))
-    (if F-p
+(defgeneric push-state (state finite-automaton &key &allow-other-keys)
+  (:documentation "Push a state into the finite automaton."))
+
+(defmethod push-state (state (Q Q) &key &allow-other-keys)
+  (with-slots (Q) Q
+    (vector-push-extend state Q)))
+
+(defmethod push-state (state (Sigma-Delta Sigma-Delta) &key &allow-other-keys)
+  (with-slots (Sigma Delta) Sigma-Delta
+    (vector-push-extend state (make-Delta Sigma) Delta)))
+
+(defmethod push-state (state (q_0 q_0) &key (start-p nil) &allow-other-keys)
+  (with-slots (q_0) q_0
+    (when start-p
+      (setf q_0 state))))
+
+(defmethod push-state (state (F F) &key (final-p nil) &allow-other-keys)
+  (with-slots (F) F
+    (if final-p
 	(vector-push-extend state F)
 	(vector-push-extend nil F))))
 
-(defmethod push-state ((state (eql 'next)) (FA FA) &key (q_0-p nil) (F-p nil))
-  (push-state (make-state FA) FA  :q_0-p q_0-p  :F-p F-p))
+(defmethod push-state ((states-names states-names) (FA FA) &key
+							     (start-p nil)
+							     (final-p nil)
+		       &allow-other-keys)
+  (call-next-method (make-state states-names) FA :start-p start-p :final-p final-p))
+
+(defun print-FA (FA)
+  (print (Q FA))
+  (print (Sigma FA))
+  (print (Delta FA))
+  (print (q_0 FA))
+  (print (F FA))
+  nil)
 
 
-;; testing out some thangs
-
-
-(defclass class-1 ()
-  ((slot-1 :initarg :slot-1
-	   :initform (list 'init-slot-1)
-	   :accessor slot-1)))
-
-(defclass class-2 (class-1)
-  ((slot-2 :initarg :slot-2
-	   :initform (list 'init-slot-2)
-	   :accessor slot-2)))
-
-
-(defgeneric func-1 (thing-1))
-
-(defmethod func-1 :around ((thing-1 class-1))
-  (with-slots (slot-1) thing-1
-    (push 'func-1-around slot-1))
-  (call-next-method))
-
-(defmethod func-1 :before ((thing-1 class-1))
-  (with-slots (slot-1) thing-1
-    (push 'func-1-before slot-1)))
-
-(defmethod func-1 ((thing-1 class-1))
-  (with-slots (slot-1) thing-1
-    (push 'func-1-prime slot-1)))
-
-(defmethod func-1 :after ((thing-1 class-1))
-  (with-slots (slot-1) thing-1
-    (push 'func-1-after slot-1)))
-
-
-(defgeneric func-2 (thing-1 thing-2))
-
-(defmethod func-2 :around (thing-1 (thing-2 class-2))
-  (with-slots (slot-1 slot-2) thing-2
-    (push 'func-2-around slot-1)
-    (push 'func-2-around slot-2))
-  (call-next-method))
-
-(defmethod func-2 :before (thing-1 (thing-2 class-2))
-  (with-slots (slot-1 slot-2) thing-2
-    (push 'func-2-before slot-1)
-    (push 'func-2-before slot-2)))
-
-(defmethod func-2 (thing-1 (thing-2 class-2))
-  (with-slots (slot-1 slot-2) thing-2
-    (push 'func-2-prime slot-1)
-    (push 'func-2-prime slot-2)))
-
-(defmethod func-2 :after (thing-1 (thing-2 class-2))
-  (with-slots (slot-1 slot-2) thing-2
-    (push 'func-2-after slot-1)
-    (push 'func-2-after slot-2)))
-
-(defmethod func-2 ((thing-1 (eql 'blah)) (thing-2 class-2))
-  (with-slots (slot-1 slot-2) thing-2
-    (push 'func-2-prime-blah slot-1)
-    (push 'func-2-prime-blah slot-2))
-  (call-next-method))
-
-
-(defgeneric func-3 (thing))
-
-(defmethod func-3 :around ((thing-1 class-1))
-  (with-slots (slot-1) thing-1
-    (push 'func-3-around slot-1))
-  (call-next-method))
-
-(defmethod func-3 :before ((thing-1 class-1))
-  (with-slots (slot-1) thing-1
-    (push 'func-3-before slot-1)))
-
-(defmethod func-3 ((thing-1 class-1))
-  (with-slots (slot-1) thing-1
-    (push 'func-3-prime slot-1)))
-
-(defmethod func-3 :after ((thing-1 class-1))
-  (with-slots (slot-1) thing-1
-    (push 'func-3-after slot-1)))
-
-(defmethod func-3 :around ((thing-2 class-2))
-  (with-slots (slot-1 slot-2) thing-2
-    (push 'func-3-around-2 slot-1)
-    (push 'func-3-around-2 slot-2))
-  (call-next-method))
-
-(defmethod func-3 :before ((thing-2 class-2))
-  (with-slots (slot-1 slot-2) thing-2
-    (push 'func-3-before-2 slot-1)
-    (push 'func-3-before-2 slot-2)))
-
-(defmethod func-3 ((thing-2 class-2))
-  (with-slots (slot-1 slot-2) thing-2
-    (push 'func-3-prime-2 slot-1)
-    (push 'func-3-prime-2 slot-2)))
-
-(defmethod func-3 :after ((thing-2 class-2))
-  (with-slots (slot-1 slot-2) thing-2
-    (push 'func-3-after-2 slot-1)
-    (push 'func-3-after-2 slot-2)))
-
-
-(defun test-func-1 ()
-  (let ((thing-class-1 (make-instance 'class-1)))
-    (func-1 thing-class-1)
-    (slot-1 thing-class-1)))
-
-(defun test-func-2 ()
-  (let ((thing-class-1 (make-instance 'class-1))
-	(thing-class-2 (make-instance 'class-2)))
-    (func-2 thing-class-1 thing-class-2)
-    (list (slot-1 thing-class-1)
-	  (slot-2 thing-class-2))))
-
-(defun test-func-3 ()
-  (let ((thing-class-1 (make-instance 'class-1))
-	(thing-class-2 (make-instance 'class-2)))
-    (func-3 thing-class-1)
-    (func-3 thing-class-2)
-    (list (list 'thing-class-1 (slot-1 thing-class-1))
-	  (list 'thing-class-2 (slot-1 thing-class-2) (slot-2 thing-class-2)))))
