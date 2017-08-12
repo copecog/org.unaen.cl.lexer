11e5cc1aeaf30b3e9eea006ba3c6cc8d4f110094
Christopher H Cope <copecd@gmail.com>
2017-08-12 11:50:31 -0600

    working again

--- lexer.lisp
+++ lexer.lisp
@@ -8,6 +8,8 @@
 ;;   an initial (or start) state q0 ∈ Q
 ;;   a set of states F distinguished as accepting (or final) states F ⊆ Q.
 
+(defvar *test-fa*)
+
 (defclass preface ()
   ((preface :initarg :preface
 	    :reader preface)))
@@ -39,15 +41,17 @@
 (defclass Sigma-Delta (Sigma Delta) ())
 
 (defclass q_0 ()
-  ((q_0 :initarg q_0
+  ((q_0 :initarg :q_0
 	:reader q_0)))
 
 (defclass F ()
-  ((F :initarg F
+  ((F :initarg :F
       :initform (make-vector 1)
       :reader F)))
 
-(defclass FA (Q Sigma-Delta q_0 F states-names) ())
+(defclass ender () ())
+
+(defclass FA (Q Sigma-Delta q_0 F states-names ender) ())
 
 (defun make-vector (size &key (initial-element nil) (adjustable t) (fill-pointer 0))
   (make-array size
@@ -68,30 +72,34 @@
   (with-slots (preface iterate) states-names
     (format nil "~a~d" preface (1- (incf iterate)))))
 
-;(defmethod initialize-instance :after ((FA FA) &key &allow-other-keys)
-;  (push-state FA FA :start-p t))
+(defmethod initialize-instance :after ((FA FA) &key &allow-other-keys)
+  (push-state FA FA :start-p t))
 
 (defgeneric push-state (state finite-automaton &key &allow-other-keys)
   (:documentation "Push a state into the finite automaton."))
 
 (defmethod push-state (state (Q Q) &key &allow-other-keys)
   (with-slots (Q) Q
-    (vector-push-extend state Q)))
+    (vector-push-extend state Q))
+  (call-next-method))
 
 (defmethod push-state (state (Sigma-Delta Sigma-Delta) &key &allow-other-keys)
   (with-slots (Sigma Delta) Sigma-Delta
-    (vector-push-extend state (make-Delta Sigma) Delta)))
+    (vector-push-extend (make-Delta Sigma) Delta))
+  (call-next-method))
 
 (defmethod push-state (state (q_0 q_0) &key (start-p nil) &allow-other-keys)
   (with-slots (q_0) q_0
     (when start-p
-      (setf q_0 state))))
+      (setf q_0 state)))
+  (call-next-method))
 
 (defmethod push-state (state (F F) &key (final-p nil) &allow-other-keys)
   (with-slots (F) F
     (if final-p
 	(vector-push-extend state F)
-	(vector-push-extend nil F))))
+	(vector-push-extend nil F)))
+  (call-next-method))
 
 (defmethod push-state ((states-names states-names) (FA FA) &key
 							     (start-p nil)
@@ -99,6 +107,8 @@
 		       &allow-other-keys)
   (call-next-method (make-state states-names) FA :start-p start-p :final-p final-p))
 
+(defmethod push-state (state (object ender) &key &allow-other-keys)
+  object)
 
 (defun print-FA (FA)
   (print (Q FA))
