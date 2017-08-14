dee141d031af76102ec3d23335a9746840557b0a
Christopher H Cope <copecd@gmail.com>
2017-08-14 11:41:33 -0600

    Not working, but progress.

--- lexer.lisp
+++ lexer.lisp
@@ -8,7 +8,7 @@
 
 (defclass iterate ()
   ((iterate :initarg :iterate
-            :reader iterate)))
+	    :reader iterate)))
 
 (defclass states-names (preface iterate)
   ((preface :initform "q_")
@@ -17,7 +17,7 @@
 
 (defclass Q ()
   ((Q :initarg :Q
-      :initform (make-vector 1)
+      :initform (make-state-vector 1)
       :reader Q
       :documentation "A finite set of states Q.")))
 
@@ -27,31 +27,40 @@
 	  :reader Sigma
 	  :documentation "A finite set of input symbols Σ.")))
 
-(defclass Delta (Q Sigma)
+(defclass Delta ()
   ((Delta :initarg :Delta
-	  :initform (make-vector 1)
+	  :initform (make-state-vector 1)
 	  :reader Delta
 	  :documentation "A transition function Δ : Q × Σ → P(Q).")))
 
-(defclass q_0 (Q)
+(defclass Q-Sigma-Delta (Q Sigma Delta) ()
+  (:documentation "Q, Sigma, and Delta a priori for complete Delta meaning."))
+
+(defclass q_0 ()
   ((q_0 :initarg :q_0
 	:reader q_0
-	:documentation "An initial (or start) state q0 ∈ Q.")))
+	:documentation "An initial (or start) state q_0 ∈ Q.")))
 
-(defclass F (Q)
+(defclass Q-q_0 (Q q_0) ()
+  (:documentation "Q and q_0 ∈ Q a priori for complete q0 meaning."))
+
+(defclass F ()
   ((F :initarg :F
-      :initform (make-vector 1)
+      :initform (make-state-vector 1)
       :reader F
       :documentation "A set of states F distinguished as accepting (or final) states F ⊆ Q.")))
 
-(defclass FA (Q Sigma Delta q_0 F)
+(defclass Q-F (Q F) ()
+  (:documentation "Q and F a priori for complete F meaning."))
+
+(defclass FA (Q-Sigma-Delta Q-q_0 Q-F)
   ((DSN :initarg DSN
 	:initform (make-instance 'states-names)
 	:reader DSN
-	:documentation "Default States Names."))
-  (:documentation "An NFA is represented formally by a 5-tuple, (Q, Σ, Δ, q0, F)."))
+	:documentation "_D_efault _S_tates _N_ames."))
+  (:documentation "An FA is represented formally by a 5-tuple, (Q, Σ, Δ, q0, F)."))
 
-(defun make-vector (size &key (initial-element nil) (adjustable t) (fill-pointer 0))
+(defun make-state-vector (size &key (initial-element nil) (adjustable t) (fill-pointer 0))
   (make-array size
 	      :initial-element initial-element
 	      :adjustable adjustable
@@ -63,40 +72,61 @@
 (defmethod make-Delta ((Sigma-type (eql 'cl-utf)))
   (make-hash-table :test 'eql)) ;eql for cl char's
 
-(defgeneric make-state (states-names)
+(defgeneric make-state-name (states-names)
   (:documentation "Name a new state from a reference class instance for a series of states."))
 	      
-(defmethod make-state ((states-names states-names))
-  (with-slots (preface iterate) states-names
+(defmethod make-state-name ((states-names-instance states-names))
+  (with-slots (preface iterate) states-names-instance
     (format nil "~a~d" preface (1- (incf iterate)))))
 
-(defmethod initialize-instance :after ((FA FA) &key &allow-other-keys)
-  (push-state (make-state (DSN FA)) FA :start-p t))
+(defmethod initialize-instance :after ((FA-instance FA)
+				       &key &allow-other-keys)
+  (push-state (make-state-name (DSN FA-instance)) FA-instance :start-p t))
 
-(defgeneric push-state (state finite-automaton &key &allow-other-keys)
+(defgeneric push-state (state-name finite-automaton
+			&key &allow-other-keys)
   (:documentation "Push a state into the finite automaton."))
 
-(defmethod push-state :before (state (Q Q) &key &allow-other-keys)
-  (with-slots (Q) Q
-    (vector-push-extend state Q)))
+(defmethod push-state :before (state-name (Q-instance Q)
+			       &key &allow-other-keys)
+  (with-slots (Q-slot) Q-instance
+    (vector-push-extend state-name Q-slot))) ; Push our state-name onto our vector of state names.
 
-(defmethod push-state :before (state (Delta Delta) &key &allow-other-keys)
-  (with-slots (Sigma Delta) Delta
-    (vector-push-extend (make-Delta Sigma) Delta)))
+(defmethod push-state :before (state-name (Q-Sigma-Delta-instance Q-Sigma-Delta)
+			       &key &allow-other-keys)
+  (with-slots (Sigma-slot Delta-slot) Q-Sigma-Delta-instance
+    (vector-push-extend (make-Delta Sigma-slot) Delta-slot))) ; Extend Delta for state-name.
 
-(defmethod push-state :before (state (q_0 q_0) &key (start-p nil) &allow-other-keys)
-  (with-slots (q_0) q_0
+(defmethod push-state :before (state-name (Q-q_0-instance Q-q_0)
+			       &key (start-p nil) &allow-other-keys)
+  (with-slots (q_0-slot) Q-q_0-instance
     (when start-p
-      (setf q_0 state))))
+      (setf q_0-slot state-name)))) ; If start state then set as start state.
 
-(defmethod push-state :before (state (F F) &key (final-p nil) &allow-other-keys)
-  (with-slots (F) F
+(defmethod push-state :before (state-name (Q-F-instance Q-F)
+			       &key (final-p nil) &allow-other-keys)
+  (with-slots (F-slot) Q-F-instance
     (if final-p
-	(vector-push-extend state F)
-	(vector-push-extend nil F))))
+	(vector-push-extend state-name F-slot) ;   Either it is a final state
+	(vector-push-extend nil F-slot))))     ; or it is not - so empty space instead.
 
-(defmethod push-state (state (FA FA) &key &allow-other-keys)
-  FA)
+(defmethod push-state (state (FA-instance FA) &key &allow-other-keys)
+  FA-instance) ; Return mutated FA after all said and done.
+
+(defclass NFA (FA) ()
+  (:documentation "Non-deterministic Finite Automata."))
+
+;; The notion of pushing it into the object rather than onto a stack.
+(defgeneric push-transit (state-A state-B transit-char FA &key &allow-other-keys)
+  (:documentation "Push a single transition on trans-char from state-A to state-B in finite automaton."))
+
+(defmethod push-transit ((state-A integer) (state-B integer) transit-char (Delta Q-Sigma-Delta)
+			 &key &allow-other-keys)
+  (with-slots (
+
+
+
+
 
 (defun print-FA (FA)
   (print (Q FA))
@@ -105,7 +135,3 @@
   (print (q_0 FA))
   (print (F FA))
   nil)
-
-(defclass NFA (FA) ())
-
-;(defgeneric set-transit (state-A
